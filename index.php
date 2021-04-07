d0f08ecf7e52f95efd93a3aebca7f14ec8749154
vupa2 <anhvu.pa96@gmail.com>
2021-04-07 16:58:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dungduong
+ * Date: 10/27/2018
+ * Time: 4:42 PM
+ */
+
+include './src/FizzBuzz.php';
+
+$fizzBuzz = new FizzBuzz(15);
+
+echo $fizzBuzz;
---
+++ src/FizzBuzz.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: dungduong
+ * Date: 10/27/2018
+ * Time: 4:36 PM
+ */
+
+class FizzBuzz
+{
+    public $status;
+
+    public function __construct($number)
+    {
+        $isFizz = $number % 3 == 0;
+        $isBuzz = $number % 5 == 0;
+        if ($isFizz && $isBuzz) {
+            $this->status =  "FizzBuzz";
+        } elseif ($isFizz) {
+            $this->status = "Fizz";
+        } elseif ($isBuzz) {
+            $this->status = "Buzz";
+        } else {
+            $this->status =  $number . "";
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->status;
+    }
+}
